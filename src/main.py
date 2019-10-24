707c6743ca4a05d8510dad918c13c712c5d61772
BubaDubaKuba <BubaDubaKuba@yandex.ru>
2019-10-24 23:37:02 +0300

    A lot of stuff

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 # TODO: add app icon
 
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QLabel, QFrame
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QHBoxLayout, QPushButton, QLabel, QFrame, QTextEdit, QPlainTextEdit, QSpacerItem
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QUrl, QRect, QPoint, QSize, Qt
 #import PyQt5
 import os
 import re
+#import output
 
 class SergeyBot:
     """Класс, отвечающий за "понимание" ввода пользователя.
@@ -23,17 +24,23 @@ class SergeyBot:
             pass
     
 
+class InputButton(QPushButton):
+    def __init__(self, str, parent=None):
+        super().__init__(str, parent=parent)
+        self.clicked.connect(lambda: print(self.text()))
+
+
 class SergeyApp(QWidget):
     """Класс основного приложения.
     Тут располагаются все виджеты(кнопки, поля ввода и др.)"""
     def __init__(self):
         super().__init__()
-        self.showing_web = True
         self.initUI()
     
     def initUI(self):
         """Функция инициализации всех виджетов
         (вызывать её не надо)"""
+
 
         # showMaximized() изменяет размер окна на максимальный.
         self.showMaximized()
@@ -42,41 +49,35 @@ class SergeyApp(QWidget):
         # Должно менять иконку приложения (Почему-то не работает).
         self.setWindowIcon(QIcon("icon.ico"))
 
+        
+
         # QWebEngineView - класс библиотеки, с помощью которого можно
         # отображать Web страницы (сайты из интернета или
         # просто локальные файлы HTML).
         # Иницилиализируем его, передавая обьект нашего главного класса.
-        self.web = QWebEngineView(self)
+        web = QWebEngineView(self)
         # Получаем абсолютный путь файла
         # map2.html (почему-то QWebEngineView
         # загружает только абсолютные пути файла).
         file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "map2.html"))
-        self.web.load(QUrl.fromLocalFile(file_path))
-        
-        def btn_clicked():
-            # Функция, которая вызывается при клике кнопки btn (пока для теста).
-            if self.showing_web:
-                # Функции
-                # QWebEngineView.hide() и
-                # QWebEngineView.show() "прячут" и показывают
-                # окно страницы с картой соответственно.
-                self.web.hide()
-                self.showing_web = False
-            else:
-                self.web.show()
-                self.showing_web = True
-        # Инициализируем кнопку, с надписью "Butzon" на ней
-        # и передаем наше основное окно(чтобы библиотека лучше понимала,
-        # как рисовать кнопку).
-        self.btn = QPushButton("Butzon", self)
-        # Передаём ей функцию, которая вызывается при клике на кнопку.
-        self.btn.clicked.connect(btn_clicked)
+        web.load(QUrl.fromLocalFile(file_path))
 
-        self.frame = QFrame(self)
-        self.frame.setFrameStyle(QFrame.Box)
-        self.frame.setStyleSheet("background-color: Gainsboro")
-        self.lbl = QLabel("reerrer", self.frame)
-        self.lbl.setWordWrap(True)
+        frame = QFrame(self)
+        frame.setFrameStyle(QFrame.Box)
+
+        inputgrid = QGridLayout(frame)
+        
+        inputButtonTexts = ["Butzon", "Futzon", "Kutson"]
+        buttons = []
+        for i in range(len(inputButtonTexts)):
+            buttons.append(InputButton(inputButtonTexts[i], frame))
+            inputgrid.addWidget(buttons[i], 0, i, 1, 1)
+        self.inputbox = QPlainTextEdit("", frame)
+        inputgrid.addWidget(self.inputbox, 1, 0, 1, len(buttons))
+        self.inputbox.setPlainText("")
+
+        lbl = QLabel("reerrer", self)
+        lbl.setWordWrap(True)
         
         # QGridLayout - один из макетных классов, которые помогает управлять
         # положением виджетов. Этот класс управляет положениями виджетов
@@ -90,25 +91,52 @@ class SergeyApp(QWidget):
         # | Поле ввода +----------------+
         # |            | Какой-то текст |
         # +------------+----------------+
-        self.main_layout = QGridLayout(self)
-        # TODO: self.main_layout.setSpacing(10)
+        main_layout = QGridLayout(self)
+
+
         # Добавляем все наши виджеты (кнопки, веб страницы и т.д.)
-        self.main_layout.addWidget(self.btn, 0, 0, Qt.AlignLeft)
-        self.main_layout.addWidget(self.frame, 1, 0)
-        self.main_layout.addWidget(self.web, 0, 1, -1, -1)
+        main_layout.addWidget(lbl,   0,  0,   70,   60)
+        main_layout.addWidget(frame, 70, 0,   30,   60)
+        main_layout.addWidget(web,   0,  60,  100,  40)
 
-        self.main_layout.setColumnStretch(0, 2)
-        self.main_layout.setColumnStretch(1, 1)
+        #self.main_layout.setColumnStretch(0, 2)
+        #self.main_layout.setColumnStretch(1, 1)
 
-        self.main_layout.setRowStretch(0, 3)
-        self.main_layout.setRowStretch(1, 1)
+        #self.main_layout.setRowStretch(0, 3)
+        #self.main_layout.setRowStretch(1, 1)
 
 
 if __name__ == "__main__":
     import sys
     print("Start!")
-    help(SergeyApp.initUI)
     app = QApplication(sys.argv)
+    app.setStyleSheet("""
+        QFrame {
+            border: 0;
+            background-color: Gainsboro;
+            margin: 10px;
+        }
+        QLabel {
+            background-color: LightGray;
+            padding: 15px;
+            font-size: 15pt;
+            qproperty-alignment: AlignLeft;
+            margin: 10px;
+        }
+        QPlainTextEdit {
+            background-color: White;
+            font-size: 15pt;
+        }
+        QPushButton {
+            background-color: Green;
+            color: White;
+            font-weight: Bold;
+            min-height: 20px;
+        }
+        QWebEngineView {
+            margin: 10px;
+        }
+    """)
     sergey = SergeyApp()
     sergey.show()
     app.exec_()
